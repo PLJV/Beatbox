37f2a3926ddf5f3c2d7e357f3e9983e506ccec25
Kyle Taylor <ktaylora@users.noreply.github.com>
2016-12-22 10:33:54 -0700

    Create CostEstimation.py

---
+++ CostEstimation.py
@@ -0,0 +1,63 @@
+from osgeo import ogr
+
+import os
+
+class Shapefile:
+
+    def __init__(self, *args):
+
+        self.data_source = None
+        self.layer = None
+        self.definition = None
+
+        self.is_valid = False # Does shapefile contain valid fields?
+
+        if args:
+            if(os.path.exists(args[0])):
+                self.open(args[0])
+
+    def open(self, path, layer=0):
+        """
+        Attempts to read-in a standard shapefile using OGR
+        """
+        try:
+            self.data_source = ogr.Open(path)
+            self.layer = self.data_source.GetLayer(layer)
+            self.definition = self.layer.GetLayerDefn()
+        except Exception as e:
+            print(e)
+
+    def check_restoration_features(self):
+        """
+        sanity-checks the contents of the attribute table to ensure consistency
+        with calc_restore_cost
+        """
+        req_playa_fields = ['hydromod','farmed']
+        field_match = 0
+        try:
+            fields = []
+            for i in range(self.definition.GetFieldCount()):
+                fields.append(self.definition.GetFieldDefn(i).GetName())
+        except Exception as e:
+            print(e)
+
+        for i in range(len(req_playa_fields)):
+            if req_playa_fields[i] in fields:
+                field_match += 1
+
+        self.is_valid = (field_match == 3)
+
+
+def calc_restore_cost (hydromod, farmed, playaacres):
+    """Calculate restoration costs for a playa, given modifications"""
+…
+
+    #unit conversions
+    m2pa = 4046.86
+    cypacreft = 1613.34
+
+    playaradiusm = math.sqrt((playaacres * m2pa)/math.pi)
+    bufferacres = (80*playaradiusm + 1600)/m2pa
+
+    return (hydromod * cost_pit) + (farmed * bufferacres * cost_bufferpa) \
+        + (farmed * playaacres * cypacreft * cost_sedimentpcy)
