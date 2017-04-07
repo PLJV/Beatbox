d47f678f5433b607808ebc2930c6768845e11e54
ktaylora <kyle.a.taylor@gmail.com>
2017-04-07 15:12:14 -0600

    house keeping and generalizing our implementation a little better

--- beatbox/moving_windows_analyses.py
+++ beatbox/moving_windows_analyses.py
@@ -14,8 +14,9 @@ import sys
 from raster import *
 from scipy import ndimage
 
+
 def gen_circular_array(nPixels=None):
-    """ Make a 2-d array for buffering. It represents a circle of
+    """ make a 2-d array for buffering. It represents a circle of
     radius buffsize pixels, with 1 inside the circle, and zero outside.
     """
     kernel = None
@@ -26,110 +27,95 @@ def gen_circular_array(nPixels=None):
         kernel = (radius <= nPixels).astype(numpy.uint8)
     return kernel
 
+def _dict_to_mwindow_filename(key=None, window_size=None):
+    return(str(key)+"_"+str(window_size)+"x"+str(window_size))
 
-def mwindow(**kwargs):
-    """ Wrapper function that performs a moving window analysis on a numpy image
-    object. Function allows user to specify an ndimage filter for use on
-    image=array object
-    :param image: an integer-based numpy.array object
-    :param filter: a string specifying the type of analysis (e.g.,'sum','mean','sd')
-    :param size: scalar representing the width of the moving window (e.g., size=3; for a 3x3 window)
-    :return: numpy.array object containing the result of the moving window
+def generic_filter(r=None, destfile=None, write=True, footprint=None, overwrite=True, function=None, size=None, dtype='uint16'):
+    """ wrapper for ndimage.generic_filter that can comprehend a GeoRaster, apply a common circular buffer, and writes a numpy
+    array to disk following user specifications
     """
-
-    if type(kwargs['image']) is None:
-        raise ValueError("mwindow requires at-least an image= argument specifying an input array")
-
-    filter    = ndimage.generic_filter
-    size      = None
-    image     = None
-    dtype     = "float64"
-    f_kwargs  = dict()
-
-    for i,arg in enumerate(kwargs):
-        if arg == "filter": # the default ndimage filters are slow, but accurate. The following are fast, but controversial
-            if kwargs[arg] == "sum":
-                def filter(kwargs) : ndimage.uniform_filter(image, size=size, mode="constant") * size**2
-            if kwargs[arg] == "mean":
-                def filter(kwargs) : ndimage.uniform_filter(image, size=size, mode="constant")
-            elif kwargs[arg] == "sd":
-                # mean of square minus square of mean (requires strict enforcement of precision)
-                def filter(kwargs):
-                    c1 = ndimage.uniform_filter(image, size=size, mode='constant')
-                    c2 = ndimage.uniform_filter(image*image, size=size, mode='constant')
-                    return((c2 - (c1*c1)) ** .5)[make_mwindow_buffer]
-        elif arg == "size":
-            size = kwargs[arg]
-        elif arg == "type":
-            dtype = kwargs[arg]
-        elif arg == "image":
-            image = kwargs[arg]
-            if not issubclass(type(image), numpy.ndarray):
-                raise TypeError("image argument must be a numpy array")
-            image = numpy.array(image, dtype=dtype) # bug-fix : on a binary surface, ndimage requires floating point precision for its calculations
-        # by default, pass un-handled arguments to ndimage.generic_filter()
-        else:
-            f_kwargs[arg] = kwargs[arg]
-
-    if size is None:
-        raise ValueError("size= argument cannot be null")
-
-    # cast to float64 to force precision (ndimage intermediates will inherit from image's type)
-    image = numpy.array(image, dtype=type)
-
-    return filter(f_kwargs)
+    WRITE_FILE = ( not os.path.isfile(destfile) | overwrite ) & write & type(destfile) is not None
+    FOOTPRINT  = footprint if footprint is True else numpy.array(gen_circular_array(nPixels=size//2))
+    # lazy duck type and apply ndimage filter to user specifications
+    try:
+        image = r.raster
+    except AttributeError as e:
+        image = r
+    # wrap across ndimage.generic_filter
+    image = ndimage.generic_filter(input=numpy.array(image, dtype='uint16'),
+                                   function=function,
+                                   footprint=FOOTPRINT,
+                                   dtype='uint16'))
+    # either save to disk or return to user
+    if WRITE_FILE:
+        try:
+            r.write(dst_filname=str(destfile))
+        except Exception as e:
+            print(e)
+    else:
+        return r
 
 
 if __name__ == "__main__":
 
-    INPUT_RASTER = None
-    WINDOW_DIMS  = [] # 107 = ~1 km; 237 = ~50 kilometers
-
+    # required parameters
+    _INPUT_RASTER = None
+    _IS_NASS = True
+    _FUNCTION=numpy.sum
+    _WINDOW_DIMS = []
+    _MATCH_ARRAYS = {}
+    _TARGET_RECLASS_VALUE = 1
+    # process runtime arguments
     for i, arg in enumerate(sys.argv):
         if arg == "-r":
             INPUT_RASTER = sys.argv[i + 1]
         elif arg == "-t":
             TARGET_RECLASS_VALUE = list(map(int, sys.argv[i + 1].split(',')))
+        elif arg == "-nass":
+            IS_NASS = True
+        elif arg == "-function":
+            if re.search(sys.argv[i + 1].lower(), "sum"):
+                FUNCTION=numpy.sum
+            elif re.search(sys.argv[i + 1].lower(), "mean"):
+                FUNCTION=numpy.mean
+            elif re.search(sys.argv[i + 1].lower(), "sd"):
+                FUNCTION=numpy.std
         elif arg == "-mw":
             WINDOW_DIMS = list(map(int, sys.argv[i + 1].split(',')))
+        elif arg == "-reclass":  # e.g., "row_crop=12,34;cereal=2,3;corn=1,10"
+            classes = sys.argv[i + 1].split(";")
+            for c in classes:
+                c = c.split("=")
+                MATCH_ARRAYS[c[0]] = list(map(int, c[1].split(",")))
+    # sanity-check
     if not WINDOW_DIMS:
         raise ValueError("moving window dimensions need to be specified using the -mw argument at runtime")
     elif not INPUT_RASTER:
         raise ValueError("this analysis requires a NASS input raster specified with -r argument at runtime")
 
-    r = NassCdlRaster(file=INPUT_RASTER)
-    r.raster = numpy.array(r.raster, dtype='uint16')
+    # process any re-classification requests prior to our moving windows analysis if asked
+    if MATCH_ARRAYS:
+        if IS_NASS:
+            r = NassCdlRaster(file=_INPUT_RASTER)
+            r.raster = numpy.array(r.raster, dtype='uint16') # anticipating composition metric calculations ?
+        else:
+            r = Raster(file=_INPUT_RASTER)
+
+        print(" -- performing moving window analyses")
+
+        for m in MATCH_ARRAYS:
+            _MATCH_ARRAYS[m] = r.binary_reclass(match=_MATCH_ARRAYS[m])
+            for window in WINDOW_DIMS:
+                filename=_dict_to_mwindow_filename(key=m, window_size=window)
+                generic_filter(r=_MATCH_ARRAYS[m], function=_FUNCTION, destfile=filename)
+
 
     # assign binary 1/0 based-on corresponding (2016) NASS CDL values on the raster surface
     # that I bogarted from the 2016 raster using 'R'. We should come-up with an elegant way to
     # code this raster algebra using just the RAT data from the raster file specified at runtime.
-    print(" -- reclassifying NASS raster input data")
-    row_crop = r.binary_reclass(match=[1, 2, 5, 12, 13, 26, 41, 225, 226, 232, 237, 238, 239, 240, 254])
-    cereal   = r.binary_reclass(match=[3, 4, 21, 22, 23, 24, 27, 28, 29, 39, 226, 233, 234, 235, 236, 237, 240, 254])
-    grass    = r.binary_reclass(match=[59, 60, 176])
-    tree     = r.binary_reclass(match=[63, 70, 71, 141, 142, 143])
-    wetland  = r.binary_reclass(match=[87, 190, 195])
-
-    # moving windows analyses
-    print(" -- performing moving window analyses")
-    for i, j in enumerate(WINDOW_DIMS):
-        #row_crop_mw = mwindow(input=row_crop, size=j)
-        #r.raster=ndimage.generic_filter(input=numpy.array(r.raster,dtype='uint16'), function=numpy.sum, footprint=numpy.array(gen_circular_array(nPixels=11),dtype='uint16'))
-        r.raster = ndimage.uniform_filter(numpy.array(row_crop, dtype="float64"), size=j, mode="constant") * j ** 2
-        r.write("2016_row_crop_" + str(j) + "x" + str(j), format=gdal.GDT_UInt16)
-
-        #cereal_mw = mwindow(input=cereal, size=j)
-        r.raster = ndimage.uniform_filter(numpy.array(cereal,dtype="float64"), size=j, mode="constant") * j ** 2
-        r.write("2016_cereal_" + str(j) + "x" + str(j), format=gdal.GDT_UInt16)
-
-        #grass_mw = mwindow(input=grass, size=j)
-        r.raster = ndimage.uniform_filter(numpy.array(grass,dtype="float64"), size=j, mode="constant") * j ** 2
-        r.write("2016_grass_" + str(j) + "x" + str(j), format=gdal.GDT_UInt16)
-
-        #tree_mw = mwindow(input=tree, size=j)
-        r.raster = ndimage.uniform_filter(numpy.array(tree,dtype="float64"), size=j, mode="constant") * j ** 2
-        r.write("2016_tree_" + str(j) + "x" + str(j), format=gdal.GDT_UInt16)
-
-        #wetland_mw = mwindow(input=wetland, size=j)
-        r.raster = ndimage.uniform_filter(numpy.array(wetland, dtype="float64"), size=j, mode="constant") * j ** 2
-        r.write("2016_wetland_" + str(j) + "x" + str(j), format=gdal.GDT_UInt16)
+    # print(" -- reclassifying NASS raster input data")
+    # row_crop = r.binary_reclass(match=[1, 2, 5, 12, 13, 26, 41, 225, 226, 232, 237, 238, 239, 240, 254])
+    # cereal   = r.binary_reclass(match=[3, 4, 21, 22, 23, 24, 27, 28, 29, 39, 226, 233, 234, 235, 236, 237, 240, 254])
+    # grass    = r.binary_reclass(match=[59, 60, 176])
+    # tree     = r.binary_reclass(match=[63, 70, 71, 141, 142, 143])
+    # wetland  = r.binary_reclass(match=[87, 190, 195])
