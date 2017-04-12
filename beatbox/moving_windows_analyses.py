35733c38b8a5f72874ad70edbc60e91396fa64b9
Kyle Taylor <kyle.a.taylor@gmail.com>
2017-04-12 11:12:11 -0600

    house keeping

--- beatbox/moving_windows_analyses.py
+++ beatbox/moving_windows_analyses.py
@@ -38,7 +38,7 @@ def generic_filter(r=None, destfile=None, write=True, footprint=None, overwrite=
     array to disk following user specifications
     """
     try:
-        _WRITE_FILE = ((not os.path.isfile(destfile)) | overwrite) & write & type(destfile) is not None
+        _WRITE_FILE = ((not os.path.isfile(destfile)) | overwrite) & write & (type(destfile) is not None)
     except TypeError as e:
         _WRITE_FILE = False
     try:
@@ -68,6 +68,7 @@ def generic_filter(r=None, destfile=None, write=True, footprint=None, overwrite=
     # either save to disk or return to user
     if _WRITE_FILE:
         try:
+            destfile=destfile.replace(".tif","") # gdal will append a filename extension for us
             r.array=image
             r.write(dst_filname = str(destfile))
         except Exception as e:
@@ -75,58 +76,3 @@ def generic_filter(r=None, destfile=None, write=True, footprint=None, overwrite=
             return image
     else:
         return image
-
-
-if __name__ == "__main__":
-    # required parameters
-    _INPUT_RASTER=None
-    _IS_NASS=True
-    _FUNCTION=numpy.sum
-    _WINDOW_DIMS=[]
-    _MATCH_ARRAYS={}
-    _TARGET_RECLASS_VALUE=1
-    # process runtime arguments
-    for i, arg in enumerate(sys.argv):
-        arg.replace("--", "-")
-        if arg == "-r":
-            _INPUT_RASTER=sys.argv[i + 1]
-        elif arg == "-t":
-            _TARGET_RECLASS_VALUE=list(map(int, sys.argv[i + 1].split(',')))
-        elif arg == "-nass":
-            _IS_NASS=True
-        elif arg == "-function":
-            if re.search(sys.argv[i + 1].lower(), "sum"):
-                _FUNCTION=numpy.sum
-            elif re.search(sys.argv[i + 1].lower(), "mean"):
-                _FUNCTION=numpy.mean
-            elif re.search(sys.argv[i + 1].lower(), "sd"):
-                _FUNCTION=numpy.std
-        elif arg == "-mw":
-            _WINDOW_DIMS=list(map(int, sys.argv[i + 1].split(',')))
-        elif arg == "-reclass":  # e.g., "row_crop=12,34;cereal=2,3;corn=1,10"
-            classes=sys.argv[i + 1].split(";")
-            for c in classes:
-                c=c.split("=")
-                _MATCH_ARRAYS[c[0]]=list(map(int, c[1].split(",")))
-    # sanity-check
-    if not _WINDOW_DIMS:
-        raise ValueError("moving window dimensions need to be specified using the -mw argument at runtime")
-    elif not _INPUT_RASTER:
-        raise ValueError("this analysis requires a NASS input raster specified with -r argument at runtime")
-
-    # process any re-classification requests prior to our moving windows analysis if asked
-    if _MATCH_ARRAYS:
-        if _IS_NASS:
-            r=Raster(file=_INPUT_RASTER)
-            r.array = numpy.array(r.array, dtype='uint16') # anticipating composition metric calculations ?
-        else:
-            r=Raster(file=_INPUT_RASTER)
-
-        print(" -- performing moving window analyses")
-
-        for m in _MATCH_ARRAYS:
-            _MATCH_ARRAYS[m]=r.binary_reclass(match=_MATCH_ARRAYS[m])
-            for window in _WINDOW_DIMS:
-                filename=_dict_to_mwindow_filename(key=m, window_size=window)
-                generic_filter(r = _MATCH_ARRAYS[m], function = _FUNCTION,
-                               size = window, destfile = filename)
