5bfc46655dea0b73115f1b89ce03d0049cb266d8
Kyle Taylor <kyle.a.taylor@gmail.com>
2018-09-11 19:21:41 -0600

    just kidding

--- scripts/convex_hulls.py
+++ scripts/convex_hulls.py
@@ -17,26 +17,40 @@ from beatbox import Vector
 
 _DEFAULT_BUFFER_WIDTH = 1000 # default width (in meters) of a geometry for various buffer operations
 
-def _dissolve_by_explode(buffers):
-    # assign an arbitrary group to all of our
-    # input polygons and dissolve
-    buffers.loc[:, "group"] = 1
-    dissolved_buffers = buffers.dissolve(by="group")
-    # now explode the dissolve from a multipart geometry into
-    # individual Polygons
-    dissolved_buffers = dissolved_buffers.\
-        explode().\
-        reset_index().rename(columns={0: 'geometry'})
+def _dissolve_overlapping_geometries(buffers):
+    # force casting as a GeoDataFrame
+    try:
+        buffers = gp.GeoDataFrame(geometry=buffers)
+    except ValueError as e:
+        if isinstance(buffers, gp.GeoDataFrame):
+            pass
+        else:
+            raise e
+    except Exception as e:
+        raise e
+    # determine appropriate groupings for our overlapping buffers
+    # 1.) using an old-ass lambda function
+    overlap_matrix = buffers.geometry.apply(lambda x: buffers.geometry.overlaps(x)).values.astype(int)
+    n, ids = connected_components(overlap_matrix)
+    # 2.) using newer listcomp hotness
+    n, ids = connected_components(
+        [ buffers.geometry.overlaps(x).values.astype(int) for x in buffers.geometry ]
+    )
     # merge attributes
-    gdf_out = dissolved_buffers.merge(buffers.drop('geometry', axis=1), left_on='level_0', right_index=True)
-    gdf_out = gdf_out.set_index(['level_0', 'level_1']).set_geometry('geometry')
-    gdf_out.crs = buffers.crs
-    gdf_out = gdf_out.reset_index()
-    return gdf_out
+    dissolved_buffers = gp.GeoDataFrame({
+        'geometry': buffers.geometry,
+        'group': ids
+    })
+    # call geopandas dissolve with our 'ids' column and return
+    # to user
+    dissolved_buffers = dissolved_buffers.dissolve(by='group')
+    dissolved_buffers.crs = buffers.crs
+
+    return dissolved_buffers
 
 def _attribute_by_overlap(buffers, points):
     # dissolve-by explode
-    gdf_out = _dissolve_by_explode(buffers)
+    gdf_out = _dissolve_overlapping_geometries(buffers)
     # return the right-sided spatial join
     return gp.\
         sjoin(points, gdf_out, how='inner', op='intersects').\
@@ -52,7 +66,7 @@ def fuzzy_convex_hulls(*args, **kwargs):
     # generate circular point buffers around our SpatialPoints features
     try:
         _point_buffers = _points
-        _point_buffers['geometry'] = _points.geometry.buffer(_width)
+        _point_buffers = _points.geometry.buffer(_width)
     # assume AttributeErrors are due to args[0] not being a GeoPandas object
     except AttributeError as e:
         # if this is a string, assume that it is a path
@@ -71,7 +85,7 @@ def fuzzy_convex_hulls(*args, **kwargs):
     point_clusters = _attribute_by_overlap(_point_buffers, _points)
 
     # build a GeoDataFrame from our dissolved buffers
-    gdf = clusters.merge(
+    gdf = point_clusters.merge(
         _point_buffers.drop('geometry', axis=1),
         left_on='level_0',
         right_index=True
